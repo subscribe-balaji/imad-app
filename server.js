1c1bda578a1ad957b351b8e3b127477faf42bf34
subscribe-balaji <31207982+subscribe-balaji@users.noreply.github.com>
2017-08-22 16:45:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 var articles = {
     'article1': {
         title: 'Article1',
