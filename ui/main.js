49223dd85f896be622ddc7da0e2fc5edab4a525c
subscribe-balaji <31207982+subscribe-balaji@users.noreply.github.com>
2017-08-22 18:10:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 var newvar = document.getElementById('main-text');
 newvar.innerHTML = "Changed text";
 
-var newvar = document.getElementById('madi');
+var newvar = document.getElementById('mdi');
 newvar.onclick = function() {
     newvar.style.marginleft = '9500px';
 };
