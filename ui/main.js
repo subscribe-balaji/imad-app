cc347bcfddb4f45b3fd1a0e3722b03635e079119
subscribe-balaji <31207982+subscribe-balaji@users.noreply.github.com>
2017-08-23 14:49:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,5 +3,5 @@ var counter = 0;
 button.onclick = function() {
     counter = counter+1;
     var span = document.getElementById('count');
-    span.innerHTML(counter.toString());
+    span.innerHTML = counter.toString();
 };
